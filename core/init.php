6c7a5a4c5885d6dc34e4928fb2b2f44ac4792720
JunakovicM <peskes.eskes@gmail.com>
2019-02-07 18:29:52 +0100

    Added error reporting

--- core/init.php
+++ core/init.php
@@ -1,7 +1,12 @@
 <?php
 
+error_reporting(E_ALL); //  E_ALL ispisuje greške dok smo u developmentu
+ini_set('display_errors', 1);
+ini_set ('display_startup_errors', true);
+
+
 session_start();
-session_regenerate_id();
+  // session_regenerate_id();
 
 spl_autoload_register(function($class) {
     require_once 'classes/' .$class . '.php';
